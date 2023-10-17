808436a58466364b20a3c9bf3355fda4f20ca06c
faresdu <443102276@student.ksu.edu.sa>
2023-10-17 03:01:29 +0300

    Full project

--- full project/Event.java
+++ full project/Event.java
@@ -1,48 +1,55 @@
-public class Event implements Comparable<Event>{
+public class Event implements Comparable<Event> {
 	private String eventTitle;
 	private String dateAndTime;
 	private String location;
 	private LinkedList<Contact> contactLinkedList;
-	
-	
+
 	public Event() {
 		this.contactLinkedList = new LinkedList<Contact>();
 		this.eventTitle = null;
 		this.dateAndTime = null;
 		this.location = null;
-		
+
 	}
+
 	public Event(String eventTitle, String dateAndTime, String location) {
 		this.eventTitle = eventTitle;
 		this.dateAndTime = dateAndTime;
 		this.location = location;
-		
+
 	}
+
 	public String getEventTitle() {
 		return eventTitle;
 	}
+
 	public void setEventTitle(String eventTitle) {
 		this.eventTitle = eventTitle;
 	}
+
 	public String getDateAndTime() {
 		return dateAndTime;
 	}
+
 	public void setDateAndTime(String dateAndTime) {
 		this.dateAndTime = dateAndTime;
 	}
+
 	public String getLocation() {
 		return location;
 	}
+
 	public void setLocation(String location) {
 		this.location = location;
 	}
-	 public Contact findContact(String data, String type) {
+
+	public Contact findContact(String data, String type) {
 		contactLinkedList.findfirst();
 		if (contactLinkedList.empty()) {
 			return null;
 		}
-		boolean last=true;
-	
+		boolean last = true;
+
 		do {
 			Contact currentContact = contactLinkedList.retrieve();
 			if (currentContact != null) {
@@ -50,7 +57,8 @@ public class Event implements Comparable<Event>{
 					return currentContact;
 				} else if (type.equalsIgnoreCase("Email") && currentContact.getEmailAddress().equalsIgnoreCase(data)) {
 					return currentContact;
-				} else if (type.equalsIgnoreCase("PhoneNumber") && currentContact.getPhoneNumber().equalsIgnoreCase(data)) {
+				} else if (type.equalsIgnoreCase("PhoneNumber")
+						&& currentContact.getPhoneNumber().equalsIgnoreCase(data)) {
 					return currentContact;
 				} else if (type.equalsIgnoreCase("Address") && currentContact.getAddress().equalsIgnoreCase(data)) {
 					return currentContact;
@@ -58,100 +66,99 @@ public class Event implements Comparable<Event>{
 					return currentContact;
 				}
 			}
-			if(last==false) {
+			if (last == false) {
 				return null;
 			}
-			
-			if(contactLinkedList.last()) {
-				last=false;
+
+			if (contactLinkedList.last()) {
+				last = false;
 				return null;
 			}
 			contactLinkedList.findnext();
-			
-		} while (!contactLinkedList.last()||last==true);
-	
+
+		} while (!contactLinkedList.last() || last == true);
+
 		return null;
 	}
+
 	public void addContact(Contact a) {
 		contactLinkedList.insert(a);
 	}
+
 	public void removeContact() {
 		contactLinkedList.remove();
 	}
-	
+
 	public void contactsNames() {
-	    System.out.println("Contact List: ");
-	    int c = 1;
+		System.out.println("Contact List: ");
+		int c = 1;
 
-	    if (contactLinkedList.empty()) {
-	        System.out.println("THERE IS NO CONTACTS");
-	        return;
-	    }
+		if (contactLinkedList.empty()) {
+			System.out.println("THERE IS NO CONTACTS");
+			return;
+		}
 
-	    contactLinkedList.findfirst();
+		contactLinkedList.findfirst();
 
-	    while(!contactLinkedList.last()){
-	        
-	    	System.out.println(c++ + " : " + contactLinkedList.retrieve().getContactName());
+		while (!contactLinkedList.last()) {
 
-	        
+			System.out.println(c++ + " : " + contactLinkedList.retrieve().getContactName());
 
-	        contactLinkedList.findnext(); }
-	    if(contactLinkedList.last())
-        	System.out.println(c++ + " : " + contactLinkedList.retrieve().getContactName());
-            
-	    
+			contactLinkedList.findnext();
+		}
+		if (contactLinkedList.last())
+			System.out.println(c++ + " : " + contactLinkedList.retrieve().getContactName());
+
 	}
+
 	public boolean isEmptyContact() {
 		return contactLinkedList.empty();
 	}
-	
-	
+
 	public void printFirstName(String name) {
-		if(contactLinkedList.empty()) {
+		if (contactLinkedList.empty()) {
 			return;
 		}
 		boolean printname = true;
 		int c = 1;
 		contactLinkedList.findfirst();
-		while(!contactLinkedList.last()) {
+		while (!contactLinkedList.last()) {
 			Contact tmp = contactLinkedList.retrieve();
-			if(tmp.getContactName().substring(0, name.length()).equalsIgnoreCase(name)) {
-				if(printname==true) {
-					System.out.println("\nEvent Name: "+getEventTitle());
+			if (name.length() <= tmp.getContactName().length() && tmp.getContactName().substring(0, name.length()).equalsIgnoreCase(name)) {
+				if (printname == true) {
+					System.out.println("\nEvent Name: " + getEventTitle());
 					System.out.println("------------------------------\n");
-					printname=false;
+					printname = false;
 				}
-				System.out.println(c+++" : "+tmp.getContactName());
-				
+				System.out.println(c++ + " : " + tmp.getContactName());
+
 			}
 			contactLinkedList.findnext();
 		}
-		if(contactLinkedList.last()) {
-			Contact tmp = contactLinkedList.retrieve();
-			if(tmp.getContactName().substring(0, name.length()).equalsIgnoreCase(name)) {
-				if(printname==true) {
-					System.out.println("Event name: "+getEventTitle());
-					System.out.println("------------------------------\n");
-					printname=false;
+			if (contactLinkedList.last()) {
+				Contact tmp = contactLinkedList.retrieve();
+				if ( name.length() <= tmp.getContactName().length() &&tmp.getContactName().substring(0, name.length()).equalsIgnoreCase(name)) {
+					if (printname == true) {
+						System.out.println("Event name: " + getEventTitle());
+						System.out.println("------------------------------\n");
+						printname = false;
+					}
+					System.out.println(c++ + " : " + tmp.getContactName());
+
 				}
-				System.out.println(c+++" : "+tmp.getContactName());
-				
 			}
+			System.out.println();
+		} 
+
+	
+
+	public int compareTo(Event o) {
+		try {
+			return (this.eventTitle.compareTo(o.getEventTitle()));
+
+		} catch (Exception e) {
+			throw new UnsupportedOperationException();
 		}
-		System.out.println();
 	}
-	public int compareTo(Event o) {  
-        try {  
-            return (this.eventTitle.compareTo(o.getEventTitle()));   
-                                                    
-        }  
-        catch (Exception e)  
-        {  
-            throw new UnsupportedOperationException();  
-        }  
-    }  
-	
-	
-	
-	}
+
+}
--- full project/mainBeta.java
+++ full project/mainBeta.java
@@ -1,149 +1,167 @@
 import java.util.Scanner;
+
 public class mainBeta {
+	private static Scanner a = new Scanner(System.in);
+	private static PhoneBook pb = new PhoneBook();
 
 	public static void main(String[] args) {
-		Scanner a = new Scanner(System.in);
-		PhoneBook pb = new PhoneBook();
 
-		/* sample tests*/
-		Contact a1 = new Contact("Faris-Aldhelan","0534335052","f@.com","RIYADH","A","2003");
-		Contact a2 = new Contact("A","0534335050","o@.com","RIYADH","A","2003");
-		Contact a3 = new Contact("Abdulaziz-Alkhinifer","0534331010","a@.com","RIYADH","A","2003");
-		Contact a4 = new Contact("Abdullah-Alhassan","051201012","a2@.com","RIYADH","A","2003");
-		Contact a5 = new Contact("B","0534332020","a3@.com","RIYADH","A","2003");
+		/* sample tests */
+		Contact a1 = new Contact("fares", "0534335052", "f@.com", "RIYADH", "A", "2003");
+		Contact a2 = new Contact("a", "0534335050", "o@.com", "RIYADH", "A", "2003");
+		Contact a3 = new Contact("Abdulaziz-Alkhinifer", "0534331010", "a@.com", "RIYADH", "A", "2003");
+		Contact a4 = new Contact("Abdullah-Alhassan", "051201012", "a2@.com", "RIYADH", "A", "2003");
+		Contact a5 = new Contact("b", "0534332020", "a3@.com", "RIYADH", "A", "2003");
 		pb.addContact(a1);
 		pb.addContact(a2);
 
 		pb.addContact(a3);
 		pb.addContact(a4);
 		pb.addContact(a5);
-		
 		System.out.println("Welcome to the Linked Tree Phonebook!");
+		int choice;
+		do {
+			mainMenu();
+			choice = a.nextInt();
+			switch (choice) {
+				case 1:
+					addContact();
+					break;
+				case 2:
+					searchContact();
+					break;
+				case 3:
+					deleteContact();
+					break;
+				case 4:
+					scheduleEvent();
+					break;
+				case 5:
+					searchEvent();
+					break;
+				case 6:
+					searchByFirstName();
+					break;
+				case 7:
+					pb.PrintEvent();
+					break;
+				case 8:
+					System.out.println("Thank you!");
+					break;
+				default:
+					System.out.println("Wrong input, Please try again. ");
+					break;
+			}
+		} while (choice != 8);
+
+	}
+
+	public static void addContact() {
+		Contact tmp = new Contact();
+		System.out.print("Enter the contact's name: ");
+		tmp.setContactName(a.next());
+
+		System.out.print("Enter the contact's phone number: ");
+		tmp.setPhoneNumber(a.next());
+
+		System.out.print("Enter the contact's Email address: ");
+		tmp.setEmailAddress(a.next());
+
+		System.out.print("Enter the contact's birthday: ");
+		tmp.setBirthday(a.next());
+
+		System.out.print("Enter the contact's address: ");
+		tmp.setAddress(a.next());
+
+		System.out.print("Enter any notes for the contact: ");
+		tmp.setNotes(a.next());
+
+		pb.addContact(tmp);
+	}
+
+	public static void searchContact() {
+		System.out.println("Enter search criteria:");
+		System.out.println("1. Name\r\n"
+				+ "2. Phone Number\r\n"
+				+ "3. Email Address\r\n"
+				+ "4. Address\r\n"
+				+ "5. Birthday\r\n");
+		System.out.print("Enter your choice:");
+		int scan2 = a.nextInt();
+		if (scan2 == 1) {
+			System.out.print("Enter the contact's name: ");
+			pb.searchContact(a.next(), "Name");
+
+		} else if (scan2 == 2) {
+			System.out.print("Enter the contact's Phone Number: ");
+			pb.searchContact(a.next(), "PhoneNumber");
+		} else if (scan2 == 3) {
+			System.out.print("Enter the contact's Email Adress: ");
+			pb.searchContact(a.next(), "Email");
+		} else if (scan2 == 4) {
+			System.out.print("Enter the contact's Address: ");
+			pb.searchContact(a.next(), "Address");
+
+		} else if (scan2 == 5) {
+			System.out.print("Enter the contact's Birtday: ");
+			pb.searchContact(a.next(), "Birthday");
+
+		}
+	}
+
+	public static void deleteContact() {
+		System.out.print("Enter the contact's phone number: ");
+		pb.deleteContact(a.next());
+	}
+
+	public static void scheduleEvent() {
+		Event e1 = new Event();
+		System.out.print("Enter event title: ");
+		e1.setEventTitle(a.next());
+
+		System.out.print("Enter contact name: ");
+		Contact tmp = pb.findContact(a.next(), "Name");
+
+		System.out.print("Enter event date and time (MM/DD/YYYY HH:MM): ");
+		e1.setDateAndTime(a.next());
+		System.out.print("Enter event location: ");
+		e1.setLocation(a.next());
+
+		pb.scheduleEvent(e1, tmp);
+	}
+
+	public static void searchEvent() {
+		System.out.println("Enter search criteria: ");
+		System.out.println("1.contact name.");
+		System.out.println("2.Event title.\n");
+		System.out.print("Enter your choice: ");
+		int a51 = a.nextInt();
+		if (a51 == 1) {
+			System.out.print("Enter the contact name: ");
+			pb.SearchEvent(a.next(), "Name");
+
+		} else if (a51 == 2) {
+			System.out.print("Enter the event title: ");
+			pb.SearchEvent(a.next(), "Title");
+
+		}
+	}
+
+	public static void searchByFirstName() {
+		System.out.print("Enter the first name: ");
+		pb.searchByFirstName(a.next());
+	}
+
+	public static void mainMenu() {
+		System.out.println("\n-------------------------");
 		System.out.println("Please choose an option:");
 		System.out.println("1. Add a contact");
 		System.out.println("2. Search for a contact");
-		System.out.println("3. Delete a contact\n4. Schedule an event\n5. Print event details\n6. Print contacts by first name");
+		System.out.println(
+				"3. Delete a contact\n4. Schedule an event\n5. Print event details\n6. Print contacts by first name ");
 		System.out.println("7. Print all events alphabetically");
-		System.out.println("8.Exit");
+		System.out.println("8. Exit");
 		System.out.println();
 		System.out.print("Enter your choice:");
-		int c = a.nextInt();
-		while(c!=8){
-			
-			if(c==1) {
-			Contact tmp = new Contact();
-			System.out.print("Enter the contact's name: ");
-			tmp.setContactName(a.next());
-			
-			System.out.print("Enter the contact's phone number: ");
-			tmp.setPhoneNumber(a.next());
-			
-			System.out.print("Enter the contact's Email address: ");
-			tmp.setEmailAddress(a.next());
-			
-			System.out.print("Enter the contact's birthday: ");
-			tmp.setBirthday(a.next());
-			
-			System.out.print("Enter the contact's address: ");
-			tmp.setAddress(a.next());
-			
-			System.out.print("Enter any notes for the contact: ");
-			tmp.setNotes(a.next());
-			
-			pb.addContact(tmp);
-			}
-			else if(c==2) {
-				System.out.println("Enter search criteria:");
-				System.out.println("1. Name\r\n"
-						+ "2. Phone Number\r\n"
-						+ "3. Email Address\r\n"
-						+ "4. Address\r\n"
-						+ "5. Birthday\r\n");
-				System.out.print("Enter your choice:");
-				int scan2 = a.nextInt();
-				if(scan2==1) {
-				System.out.print("Enter the contact's name: ");
-				pb.searchContact(a.nextLine(), "Name");
-				
-				}
-				else if(scan2==2) {
-				System.out.print("Enter the contact's Phone Number: ");
-				pb.searchContact(a.next(), "PhoneNumber");	
-				}
-				else if(scan2==3) {
-				System.out.print("Enter the contact's Email Adress: ");
-				pb.searchContact(a.next(), "Email");
-				}
-				else if(scan2==4) {
-				System.out.print("Enter the contact's Address: ");
-				pb.searchContact(a.next(), "Address");
-				
-				}
-				else if(scan2==5){
-				System.out.print("Enter the contact's Birtday: ");
-				pb.searchContact(a.next(), "Birthday");	
-				
-				}
-				
-			} 
-			else if(c==3) {
-				System.out.print("Enter the contact's phone number: ");
-				pb.deleteContact(a.next());
-			}
-			else if(c==4) {
-				Event e1 = new Event();
-				System.out.print("Enter event title: ");
-				e1.setEventTitle(a.next());
-				
-				System.out.print("Enter contact name: ");
-				Contact tmp = pb.findContact(a.next(), "Name");
-				
-				System.out.print("Enter event date and time (MM/DD/YYYY HH:MM): ");
-				e1.setDateAndTime(a.next());
-				System.out.print("Enter event location: ");
-				e1.setLocation(a.next());
-				
-				pb.scheduleEvent(e1,tmp);
-			}
-			else if(c==5) {
-				System.out.println("Enter search criteria: ");
-				System.out.println("1.contact name.");
-				System.out.println("2.Event title.\n");
-				System.out.print("Enter your choice: ");
-				int a51=a.nextInt();
-			   if(a51==1){
-					System.out.print("Enter the contact name: ");
-					pb.SearchEvent(a.next(), "Name");
-					
-				}
-			   else if(a51==2){
-					System.out.print("Enter the event title: ");
-					pb.SearchEvent(a.next(), "Title");
-					
-				}
-				
-			}
-			else if(c==6) {
-				System.out.print("Enter the first name: ");
-				pb.searchByFirstName(a.next());
-				
-			}
-			else if(c==7) {
-				pb.PrintEvent();
-			}
-			System.out.println("\n-------------------------");
-			System.out.println("Please choose an option:");
-			System.out.println("1. Add a contact");
-			System.out.println("2. Search for a contact");
-			System.out.println("3. Delete a contact\n4. Schedule an event\n5. Print event details\n6. Print contacts by first name ");
-			System.out.println("7. Print all events alphabetically");
-			System.out.println("8. Exit");
-			System.out.println();
-			System.out.print("Enter your choice:");
-			c = a.nextInt();
-			}
-		System.out.println("THANK YOU");
-		
 	}
-
 }
